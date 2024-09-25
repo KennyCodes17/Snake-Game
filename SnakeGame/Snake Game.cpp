bcd8a3af350815f786d0a27cb4a921da31366482
KennyCodes17 <kennycrc7@gmail.com>
2024-09-25 15:13:07 -0700

    snake growth

--- SnakeGame/Snake Game.cpp
+++ SnakeGame/Snake Game.cpp
@@ -37,11 +37,13 @@ void getNextAppleCoordinates(int& appleX, int& appleY)
 	appleY = rand() % (height - 2) + 1;
 }
 
-void setup()
+void setup(vector<pair<int, int>>& snake)
 {
 	hideCursor();
 	gameOver = false;
 	dir = STOP;
+
+	snake.push_back(make_pair(width / 2, height / 2));
 
 }
 
@@ -68,6 +70,11 @@ bool checkBorder(int x, int y)
 	return false;
 }
 
+bool checkSnakeSelfCollision(vector<pai<int, int>>& snake)
+{
+
+}
+
 void print(std::string s)
 {
 	std::cout << s;
@@ -87,38 +94,45 @@ bool drawBorder(int x, int y)
 	return false;
 }
 
-bool drawSnake(int x, int y, int snakeX, int snakeY)
+bool drawSnake(int x, int y, vector<pair<int, int>>& snake)
 {
-	if (x == snakeX && y == snakeY)
+	for (int i = 0; i < snake.size(); i++)
 	{
-		print("s");
-		return true;
+		if (x == snake[i].first && y == snake[i].second)
+		{
+			print("s");
+			return true;
+		}
 	}
-
 	return false;
+}
+
+void addSnakeSegment(vector<pair<int, int>>& snake)
+{
+	snake.push_back(snake.back());
 }
 
 bool drawApple(int x, int y, int appleX, int appleY)
 {
 	if (x == appleX && y == appleY)
 	{
-		print("A");
+		print("a");
 		return true;
 	}
 
 	return false;
 }
 
-bool appleCollision(int appleX, int appleY, int snakeX, int snakeY)
+bool appleCollision(int appleX, int appleY, vector<pair<int, int>>& snake)
 {
-	if (appleX == snakeX && appleY == snakeY)
+	if (appleX == snake[0].first && appleY == snake[0].second)
 	{
 		return true;
 	}
 	return false;
 }
 
-void draw(int snakeX, int snakeY, int appleX, int appleY)
+void draw(int appleX, int appleY, vector<pair<int, int>>& snake)
 {
 	setCursorPosition(0, 0);
 
@@ -128,7 +142,7 @@ void draw(int snakeX, int snakeY, int appleX, int appleY)
 		for (int x = 0; x < width; x++)
 		{
 			if (!drawBorder(x, y)
-				&& !drawSnake(x, y, snakeX, snakeY)
+				&& !drawSnake(x, y, snake)
 				&& !drawApple(x, y, appleX, appleY))
 			{
 				print(" ");
@@ -155,40 +169,55 @@ void input()
 	}
 }
 
-void logic(int& appleX, int& appleY, int& snakeX, int& snakeY)
+void logic(int& appleX, int& appleY, vector<pair<int, int>>& snake)
 {
+	for (int i = snake.size() - 1; i > 0; i--)
+	{
+		snake[i] = snake[i - 1];
+	}
+
 	switch (dir)
 	{
+	//Y coordinate stored in second element of the vector
 	case UP:
-		snakeY--;
+		snake[0].second--;
 		break;
+	//Y coordinate stored in second element of the vector
 	case DOWN:
-		snakeY++;
+		snake[0].second++;
 		break;
+	//X coordinate stored in first element of the vector
 	case LEFT:
-		snakeX--;
+		snake[0].first--;
 		break;
+	//X coordinate stored in first element of the vector
 	case RIGHT:
-		snakeX++;
+		snake[0].first++;
 		break;
 	default:
 		break;
 	}
 
-	if (checkBorder(snakeX, snakeY))
+	//END GAME if collides with border
+	if (checkBorder(snake[0].first, snake[0].second))
 	{
 		gameOver = true;
 	}
 
-	if (appleCollision(appleX, appleY, snakeX, snakeY))
+	if (appleCollision(appleX, appleY, snake))
 	{
 		getNextAppleCoordinates(appleX, appleY);
+		addSnakeSegment(snake);
+		cout << "Apple eaten! New segment added. Total segments: " << snake.size() << endl;
 	}
 }
 
 int main()
 {
-	setup();
+	//Snake Vector
+	vector<pair<int, int>> snake;
+
+	setup(snake);
 
 	int snakeX = width / 2;
 	int snakeY = height / 2;
@@ -200,8 +229,8 @@ int main()
 	while (!gameOver) 
 	{
 		input();
-		logic(appleX, appleY, snakeX, snakeY);
-		draw(snakeX, snakeY, appleX, appleY);
+		logic(appleX, appleY, snake);
+		draw(appleX, appleY, snake);
 		std::this_thread::sleep_for(std::chrono::milliseconds(200));
 	}
 
